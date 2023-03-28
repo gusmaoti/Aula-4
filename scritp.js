f050e546e1b624ccb1e425a5e433bb2466314a24
Gusmão <gusmaomatheusti@gmail.com>
2023-03-28 11:15:15 -0300

    Aula

---
+++ scritp.js
@@ -0,0 +1,19 @@
+//Substituir texto
+let texto ="Estão chegando as provas"
+console.log(texto.replace("provas","avaliações"))
+
+//Fixa o valor
+let num = 233.3494
+console.log(num.toFixed(2))
+
+//Caixa alert
+//alert("Tô com fome!")
+
+//Caixa de confirmação
+//let teste = confirm("Você está com fome?")
+//console.log(teste)
+
+//Caixa de Pompt
+let texto_1 = prompt("Digite seu nome: ")
+console.log("O nome dele é:", texto_1)
+document.write("O nome dele é: ", texto_1)
